afae53c21fe22891675aa8264ceb50f61bf827f9
Wesley McClure <wes.mcclure@gmail.com>
2014-01-13 17:50:16 -0800

    Add tests to ensure we can extend the IdentityUser and think about this going forward with the implementation

---
+++ src/IntegrationTests/EnsureWeCanExtendIdentityUserTests.cs
@@ -0,0 +1,53 @@
+﻿namespace IntegrationTests
+{
+	using System.Linq;
+	using AspNet.Identity.MongoDB;
+	using Microsoft.AspNet.Identity;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class EnsureWeCanExtendIdentityUserTests : UserIntegrationTestsBase
+	{
+		private UserManager<ExtendedIdentityUser> _Manager;
+		private ExtendedIdentityUser _User;
+
+		public class ExtendedIdentityUser : IdentityUser
+		{
+			public string ExtendedField { get; set; }
+		}
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			var context = new IdentityContext(Users);
+			var userStore = new UserStore<ExtendedIdentityUser>(context);
+			_Manager = new UserManager<ExtendedIdentityUser>(userStore);
+			_User = new ExtendedIdentityUser
+			{
+				UserName = "bob"
+			};
+		}
+
+		[Test]
+		public void Create_ExtendedUserType_SavesExtraFields()
+		{
+			_User.ExtendedField = "extendedField";
+
+			_Manager.Create(_User);
+
+			var savedUser = Users.FindAllAs<ExtendedIdentityUser>().Single();
+			Expect(savedUser.ExtendedField, Is.EqualTo("extendedField"));
+		}
+
+		[Test]
+		public void Create_ExtendedUserType_ReadsExtraFields()
+		{
+			_User.ExtendedField = "extendedField";
+
+			_Manager.Create(_User);
+
+			var savedUser = _Manager.FindById(_User.Id);
+			Expect(savedUser.ExtendedField, Is.EqualTo("extendedField"));
+		}
+	}
+}

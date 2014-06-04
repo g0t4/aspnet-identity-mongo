5de63aad8db4afaf27d1b54451b6f2d423cd3437
Wesley McClure <wes.mcclure@gmail.com>
2014-06-04 08:10:28 -0700

    Open up UserStore if anyone wants to extend it

--- src/AspNet.Identity.MongoDB/UserStore.cs
+++ src/AspNet.Identity.MongoDB/UserStore.cs
@@ -30,96 +30,96 @@
 			_Context = context;
 		}
 
-		public void Dispose()
+		public virtual void Dispose()
 		{
 			// no need to dispose of anything, mongodb handles connection pooling automatically
 		}
 
-		public Task CreateAsync(TUser user)
+		public virtual Task CreateAsync(TUser user)
 		{
 			return Task.Run(() => _Context.Users.Insert(user));
 		}
 
-		public Task UpdateAsync(TUser user)
+		public virtual Task UpdateAsync(TUser user)
 		{
 			// todo should add an optimistic concurrency check
 			return Task.Run(() => _Context.Users.Save(user));
 		}
 
-		public Task DeleteAsync(TUser user)
+		public virtual Task DeleteAsync(TUser user)
 		{
 			var queryById = Query<TUser>.EQ(u => u.Id, user.Id);
 			return Task.Run(() => _Context.Users.Remove(queryById));
 		}
 
-		public Task<TUser> FindByIdAsync(string userId)
+		public virtual Task<TUser> FindByIdAsync(string userId)
 		{
 			return Task.Run(() => _Context.Users.FindOneByIdAs<TUser>(ObjectId.Parse(userId)));
 		}
 
-		public Task<TUser> FindByNameAsync(string userName)
+		public virtual Task<TUser> FindByNameAsync(string userName)
 		{
 			// todo exception on duplicates? or better to enforce unique index to ensure this
 			var queryByName = Query<TUser>.EQ(u => u.UserName, userName);
 			return Task.Run(() => _Context.Users.FindOneAs<TUser>(queryByName));
 		}
 
-		public Task SetPasswordHashAsync(TUser user, string passwordHash)
+		public virtual Task SetPasswordHashAsync(TUser user, string passwordHash)
 		{
 			user.PasswordHash = passwordHash;
 			return Task.FromResult(0);
 		}
 
-		public Task<string> GetPasswordHashAsync(TUser user)
+		public virtual Task<string> GetPasswordHashAsync(TUser user)
 		{
 			return Task.FromResult(user.PasswordHash);
 		}
 
-		public Task<bool> HasPasswordAsync(TUser user)
+		public virtual Task<bool> HasPasswordAsync(TUser user)
 		{
 			return Task.FromResult(user.HasPassword());
 		}
 
-		public Task AddToRoleAsync(TUser user, string roleName)
+		public virtual Task AddToRoleAsync(TUser user, string roleName)
 		{
 			user.AddRole(roleName);
 			return Task.FromResult(0);
 		}
 
-		public Task RemoveFromRoleAsync(TUser user, string roleName)
+		public virtual Task RemoveFromRoleAsync(TUser user, string roleName)
 		{
 			user.RemoveRole(roleName);
 			return Task.FromResult(0);
 		}
 
-		public Task<IList<string>> GetRolesAsync(TUser user)
+		public virtual Task<IList<string>> GetRolesAsync(TUser user)
 		{
 			return Task.FromResult((IList<string>) user.Roles);
 		}
 
-		public Task<bool> IsInRoleAsync(TUser user, string roleName)
+		public virtual Task<bool> IsInRoleAsync(TUser user, string roleName)
 		{
 			return Task.FromResult(user.Roles.Contains(roleName));
 		}
 
-		public Task AddLoginAsync(TUser user, UserLoginInfo login)
+		public virtual Task AddLoginAsync(TUser user, UserLoginInfo login)
 		{
 			user.AddLogin(login);
 			return Task.FromResult(0);
 		}
 
-		public Task RemoveLoginAsync(TUser user, UserLoginInfo login)
+		public virtual Task RemoveLoginAsync(TUser user, UserLoginInfo login)
 		{
 			user.RemoveLogin(login);
 			return Task.FromResult(0);
 		}
 
-		public Task<IList<UserLoginInfo>> GetLoginsAsync(TUser user)
+		public virtual Task<IList<UserLoginInfo>> GetLoginsAsync(TUser user)
 		{
 			return Task.FromResult((IList<UserLoginInfo>) user.Logins);
 		}
 
-		public Task<TUser> FindAsync(UserLoginInfo login)
+		public virtual Task<TUser> FindAsync(UserLoginInfo login)
 		{
 			return Task.Factory
 				.StartNew(() => _Context.Users.AsQueryable<TUser>()
@@ -127,134 +127,134 @@
 						.Any(l => l.LoginProvider == login.LoginProvider && l.ProviderKey == login.ProviderKey)));
 		}
 
-		public Task SetSecurityStampAsync(TUser user, string stamp)
+		public virtual Task SetSecurityStampAsync(TUser user, string stamp)
 		{
 			user.SecurityStamp = stamp;
 			return Task.FromResult(0);
 		}
 
-		public Task<string> GetSecurityStampAsync(TUser user)
+		public virtual Task<string> GetSecurityStampAsync(TUser user)
 		{
 			return Task.FromResult(user.SecurityStamp);
 		}
 
-		public Task<bool> GetEmailConfirmedAsync(TUser user)
+		public virtual Task<bool> GetEmailConfirmedAsync(TUser user)
 		{
 			return Task.FromResult(user.EmailConfirmed);
 		}
 
-		public Task SetEmailConfirmedAsync(TUser user, bool confirmed)
+		public virtual Task SetEmailConfirmedAsync(TUser user, bool confirmed)
 		{
 			user.EmailConfirmed = confirmed;
 			return Task.FromResult(0);
 		}
 
-		public Task SetEmailAsync(TUser user, string email)
+		public virtual Task SetEmailAsync(TUser user, string email)
 		{
 			user.Email = email;
 			return Task.FromResult(0);
 		}
 
-		public Task<string> GetEmailAsync(TUser user)
+		public virtual Task<string> GetEmailAsync(TUser user)
 		{
 			return Task.FromResult(user.Email);
 		}
 
-		public Task<TUser> FindByEmailAsync(string email)
+		public virtual Task<TUser> FindByEmailAsync(string email)
 		{
 			// todo what if a user can have multiple accounts with the same email?
 			return Task.Run(() => _Context.Users.AsQueryable<TUser>().FirstOrDefault(u => u.Email == email));
 		}
 
-		public Task<IList<Claim>> GetClaimsAsync(TUser user)
+		public virtual Task<IList<Claim>> GetClaimsAsync(TUser user)
 		{
 			return Task.FromResult((IList<Claim>) user.Claims.Select(c => c.ToSecurityClaim()).ToList());
 		}
 
-		public Task AddClaimAsync(TUser user, Claim claim)
+		public virtual Task AddClaimAsync(TUser user, Claim claim)
 		{
 			user.AddClaim(claim);
 			return Task.FromResult(0);
 		}
 
-		public Task RemoveClaimAsync(TUser user, Claim claim)
+		public virtual Task RemoveClaimAsync(TUser user, Claim claim)
 		{
 			user.RemoveClaim(claim);
 			return Task.FromResult(0);
 		}
 
-		public IQueryable<TUser> Users
+		public virtual IQueryable<TUser> Users
 		{
 			get { return _Context.Users.AsQueryable<TUser>(); }
 		}
 
-		public Task SetPhoneNumberAsync(TUser user, string phoneNumber)
+		public virtual Task SetPhoneNumberAsync(TUser user, string phoneNumber)
 		{
 			user.PhoneNumber = phoneNumber;
 			return Task.FromResult(0);
 		}
 
-		public Task<string> GetPhoneNumberAsync(TUser user)
+		public virtual Task<string> GetPhoneNumberAsync(TUser user)
 		{
 			return Task.FromResult(user.PhoneNumber);
 		}
 
-		public Task<bool> GetPhoneNumberConfirmedAsync(TUser user)
+		public virtual Task<bool> GetPhoneNumberConfirmedAsync(TUser user)
 		{
 			return Task.FromResult(user.PhoneNumberConfirmed);
 		}
 
-		public Task SetPhoneNumberConfirmedAsync(TUser user, bool confirmed)
+		public virtual Task SetPhoneNumberConfirmedAsync(TUser user, bool confirmed)
 		{
 			user.PhoneNumberConfirmed = confirmed;
 			return Task.FromResult(0);
 		}
 
-		public Task SetTwoFactorEnabledAsync(TUser user, bool enabled)
+		public virtual Task SetTwoFactorEnabledAsync(TUser user, bool enabled)
 		{
 			user.TwoFactorEnabled = enabled;
 			return Task.FromResult(0);
 		}
 
-		public Task<bool> GetTwoFactorEnabledAsync(TUser user)
+		public virtual Task<bool> GetTwoFactorEnabledAsync(TUser user)
 		{
 			return Task.FromResult(user.TwoFactorEnabled);
 		}
 
-		public Task<DateTimeOffset> GetLockoutEndDateAsync(TUser user)
+		public virtual Task<DateTimeOffset> GetLockoutEndDateAsync(TUser user)
 		{
 			return Task.FromResult(user.LockoutEndDateUtc ?? new DateTimeOffset());
 		}
 
-		public Task SetLockoutEndDateAsync(TUser user, DateTimeOffset lockoutEnd)
+		public virtual Task SetLockoutEndDateAsync(TUser user, DateTimeOffset lockoutEnd)
 		{
 			user.LockoutEndDateUtc = new DateTime(lockoutEnd.Ticks, DateTimeKind.Utc);
 			return Task.FromResult(0);
 		}
 
-		public Task<int> IncrementAccessFailedCountAsync(TUser user)
+		public virtual Task<int> IncrementAccessFailedCountAsync(TUser user)
 		{
 			user.AccessFailedCount++;
 			return Task.FromResult(user.AccessFailedCount);
 		}
 
-		public Task ResetAccessFailedCountAsync(TUser user)
+		public virtual Task ResetAccessFailedCountAsync(TUser user)
 		{
 			user.AccessFailedCount = 0;
 			return Task.FromResult(0);
 		}
 
-		public Task<int> GetAccessFailedCountAsync(TUser user)
+		public virtual Task<int> GetAccessFailedCountAsync(TUser user)
 		{
 			return Task.FromResult(user.AccessFailedCount);
 		}
 
-		public Task<bool> GetLockoutEnabledAsync(TUser user)
+		public virtual Task<bool> GetLockoutEnabledAsync(TUser user)
 		{
 			return Task.FromResult(user.LockoutEnabled);
 		}
 
-		public Task SetLockoutEnabledAsync(TUser user, bool enabled)
+		public virtual Task SetLockoutEnabledAsync(TUser user, bool enabled)
 		{
 			user.LockoutEnabled = enabled;
 			return Task.FromResult(0);
